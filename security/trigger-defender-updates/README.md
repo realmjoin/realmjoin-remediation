2f0d2e23bec6604c3fee893202537555164a4639
Steffen Schwerdtfeger <steffen.schwerdtfeger@glueckkanja-gab.com>
2023-12-02 11:50:36 +0100

    Added more output for PAR to trigger Defender updates

--- security/trigger-defender-updates/README.md
+++ security/trigger-defender-updates/README.md
@@ -2,7 +2,17 @@
 
 Triggers Defender updates using Update-MpSignature when status of "DefenderSignaturesOutOfDate" is true or updates have never been executed (during deployment/OOBE).
 
-Execution status of inital trigger is stored in registry:
+Execution status of inital trigger (OOBE) is stored in registry:
 - path: `HKLM:\SOFTWARE\RealmJoin\Custom\PAR`
 - name: `TriggeredDefenderUpdatesInOOBE`
 - value: `1`
+
+... with execution time:
+
+- path: `HKLM:\SOFTWARE\RealmJoin\Custom\PAR`
+- name: `TriggeredDefenderUpdatesInOOBETime`
+
+Last execution time outside inital deployment is stored in:
+
+- path: `HKLM:\SOFTWARE\RealmJoin\Custom\PAR`
+- name: `TriggeredDefenderUpdatesTime`
--- security/trigger-defender-updates/Remediate.ps1
+++ security/trigger-defender-updates/Remediate.ps1
@@ -13,17 +13,29 @@ try {
 
     # update status in registry
     $registryPathStatus = "HKLM:\SOFTWARE\RealmJoin\Custom\PAR"         # path
-    $registryNameStatus = "TriggeredDefenderUpdatesInOOBE"              # name
-    $registryValueStatus = 1                                                          # value
-    $registryTypeStatus = [Microsoft.Win32.RegistryValueKind]::DWord                  # type: String, ExpandString, Binary, DWord, MultiString, Qword, Unknown
+
+    $registryNameStatusOOBE = "TriggeredDefenderUpdatesInOOBE"              # name
+    $registryValueStatusOOBE = 1                                            # value
+    $registryTypeStatusOOBE = [Microsoft.Win32.RegistryValueKind]::DWord    # type: String, ExpandString, Binary, DWord, MultiString, Qword, Unknown
+
+    $registryNameStatusOOBETime = "TriggeredDefenderUpdatesInOOBETime"              # name
+    $registryValueStatusOOBETime = Get-Date -Format "yyyy-MM-dd HH:mm K"            # value
+    $registryTypeStatusOOBETime = [Microsoft.Win32.RegistryValueKind]::String       # type: String, ExpandString, Binary, DWord, MultiString, Qword, Unknown
+
+    $registryNameStatusTime = "TriggeredDefenderUpdatesTime"              # name
+    $registryValueStatusTime = Get-Date -Format "yyyy-MM-dd HH:mm K"            # value
+    $registryTypeStatusTime = [Microsoft.Win32.RegistryValueKind]::String       # type: String, ExpandString, Binary, DWord, MultiString, Qword, Unknown
+
 
     if(!(Test-Path $registryPathStatus)) {
         New-Item -Path $registryPathStatus -Force | Out-Null
-        Set-ItemProperty -Path $registryPathStatus -Name $registryNameStatus -Value $registryValueStatus -Type $registryTypeStatus -Force | Out-Null
-    } else {
-        Set-ItemProperty -Path $registryPathStatus -Name $registryNameStatus -Value $registryValueStatus -Type $registryTypeStatus -Force | Out-Null
     }
-
+    if((Get-ItemProperty -Path $registryPathStatus).$registryNameStatusOOBE -ne "1"){
+        Set-ItemProperty -Path $registryPathStatus -Name $registryNameStatusOOBE -Value $registryValueStatusOOBE -Type $registryTypeStatusOOBE -Force | Out-Null
+        Set-ItemProperty -Path $registryPathStatus -Name $registryNameStatusOOBETime -Value $registryValueStatusOOBETime -Type $registryTypeStatusOOBETime -Force | Out-Null
+    } else {
+        Set-ItemProperty -Path $registryPathStatus -Name $registryNameStatusTime -Value $registryValueStatusTime -Type $registryTypeStatusTime -Force | Out-Null
+    }
     exit 0
 } catch {
     # error occured
