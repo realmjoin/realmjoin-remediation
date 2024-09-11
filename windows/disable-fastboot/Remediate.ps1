e6383c272ddca9400bce3268cd89b25deac4bf06
Christian Kleemann <christian.kleemann@glueckkanja.com>
2024-09-11 15:05:12 +0200

    New Remediation for disabling Fastboot

---
+++ windows/disable-fastboot/Remediate.ps1
@@ -0,0 +1,29 @@
+﻿#=============================================================================================================================
+#
+# Script Name:         Remediate.ps1
+# Description:         Set registry keys
+#
+#=============================================================================================================================
+
+# define Variables
+$path = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power"
+$name = "HiberbootEnabled"
+$value = "0"
+$type = [Microsoft.Win32.RegistryValueKind]::DWord
+
+try {
+    # set registry key
+    if(!(Test-Path $path)) {
+        New-Item -Path $path -Force | Out-Null
+        Set-ItemProperty -Path $path -Name $name -Value $value -Type $type -Force | Out-Null
+    }
+    else {
+        Set-ItemProperty -Path $path -Name $name -Value $value -Type $type -Force | Out-Null
+    }
+    
+} catch {
+    # error occured
+    $errMsg = $_.Exception.Message
+    Write-Host "Error: $errMsg"
+    exit 1
+}
