38fb3fe895ecdad0d4ea479d9e3cea673eaae05e
Steffen Schwerdtfeger <steffen.schwerdtfeger@glueckkanja.com>
2025-09-23 10:18:40 +0200

    Add state details for Install capability Defender Sense Client

--- security/install-capability-defender-sense-client/Detect.ps1
+++ security/install-capability-defender-sense-client/Detect.ps1
@@ -2,18 +2,20 @@
 #
 # Script Name:         Detect.ps1
 # Description:         Check if Sense service is found
-# Changelog:           2024-08-20: Inital version.
+# Changelog:           2025-09-23: Package Status added.
+#                      2024-08-20: Inital version.
 #
 #=============================================================================================================================
 
 # Get Sense service
 $senseService = Get-Service "Sense" -ErrorAction SilentlyContinue
-
+ 
 # Check if service has been found
 if($null -eq $senseService) {
-    Write-Output "Sense service not found."
+    $sensePackage = Get-WindowsCapability -Name "Microsoft.Windows.Sense.Client~~~~" -Online
+    Write-Output ("State: " + $sensePackage.State)
     exit 1
 } else {
-    Write-Output "Sense service found. OK."
+    Write-Output "State: Present"
     exit 0
 }
