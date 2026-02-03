8cb81fd93b321be1d4d9634899c445135533df9b
Steffen Schwerdtfeger <50098820+schwerdti@users.noreply.github.com>
2026-02-03 11:42:00 +0100

    Update Remediate.ps1

--- windows/remove-learn-about-this-picture-windows-spotlight-shortcut/Remediate.ps1
+++ windows/remove-learn-about-this-picture-windows-spotlight-shortcut/Remediate.ps1
@@ -1,8 +1,8 @@
-﻿#=============================================================================================================================
+#=============================================================================================================================
 #
 # Script Name:         Remediate.ps1
-# Description:         Creates Reg key for hiding Windows spotlights "Learn about this picture" shortcut
-# Changelog:           2026-01-23 v1 created
+# Description:         Creates registry key for hiding Windows spotlight "Learn about this picture"  desktop shortcut
+# Changelog:           2026-01-23: Inital version
 # References:          ...
 # Notes:               ...
 #
@@ -12,21 +12,19 @@ $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktop
 $ValueName = "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}"
 $DesiredValue = 1
 
-
 try {
-
-  # Ensure the registry path exists
-  if (-not (Test-Path $RegPath)) {
-    New-Item -Path $RegPath -Force | Out-Null
-  }
+    # Ensure the registry path exists
+    if (-not (Test-Path $RegPath)) {
+        New-Item -Path $RegPath -Force | Out-Null
+    }
   
-  # Create or update the registry value
-  New-ItemProperty -Path $RegPath -Name $ValueName -PropertyType DWord -Value $DesiredValue -Force | Out-Null
-
-  exit 0
-} catch {
-  # error occured
-  $errMsg = $_.Exception.Message
-    Write-Host "Error: $errMsg"
-    exit 1
+    # Create or update the registry value
+    New-ItemProperty -Path $RegPath -Name $ValueName -PropertyType DWord -Value $DesiredValue -Force | Out-Null
+    #exit 0
+}
+catch {
+    # error occured
+    $errMsg = $_.Exception.Message
+    Write-Error $errMsg
+    #exit 1
 }
