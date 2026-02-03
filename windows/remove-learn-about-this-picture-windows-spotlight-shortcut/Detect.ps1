a4ab695371fb77b1919b177635b33962458cc858
Steffen Schwerdtfeger <50098820+schwerdti@users.noreply.github.com>
2026-02-03 11:39:45 +0100

    Update Detect.ps1

--- windows/remove-learn-about-this-picture-windows-spotlight-shortcut/Detect.ps1
+++ windows/remove-learn-about-this-picture-windows-spotlight-shortcut/Detect.ps1
@@ -1,9 +1,9 @@
-﻿#=============================================================================================================================
+#=============================================================================================================================
 #
 # Script Name:         Detect.ps1
 # Description:         Checks if the RegKey for hiding Windows spotlights "Learn about this picture" shortcut is present
-# Changelog:           2026-01-23 v1 created
-# References:          ...
+# Changelog:           2026-01-23: Inital version
+# References:          https://learn.microsoft.com/en-us/answers/questions/2157455/how-to-remove-a-persistent-shortcut-learn-about-th
 # Notes:               ...
 #
 #=============================================================================================================================
@@ -13,19 +13,18 @@ $ValueName = "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}"
 $DesiredValue = 1
 
 try {
-    $CurrentValue = Get-ItemProperty -Path $RegPath -Name $ValueName -ErrorAction Stop |
-        Select-Object -ExpandProperty $ValueName
+    $CurrentValue = Get-ItemProperty -Path $RegPath -Name $ValueName -ErrorAction Stop | Select-Object -ExpandProperty $ValueName
 
     if ($CurrentValue -eq $DesiredValue) {
-        Write-Output "Compliant: Registry value exists and is set to $DesiredValue"
+        Write-Output "Compliant."
         exit 0
     }
     else {
-        Write-Output "Non-compliant: Registry value exists but is not set to $DesiredValue"
+        Write-Output "Not matching desired value."
         exit 1
     }
 }
 catch {
-    Write-Output "Non-compliant: Registry path or value does not exist"
+    Write-Output "Not existing."
     exit 1
 }
